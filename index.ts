f621851b690cda2dfbfd15fe5016529ca6bd6afe
unknown <biancanicoly19@gmail.com>
2021-08-31 23:22:45 -0300

    Aprendendo...

---
+++ index.ts
@@ -0,0 +1,13 @@
+
+import caixa from './Caixa'
+
+
+let meuCaixa = new caixa('', 08, '')//instância. Uma nova estrutura da modelagem da classe(Caixa)instânciada. Um novo objeto conforme esses atributos
+
+meuCaixa.bemvindo()
+
+meuCaixa.passagem(1248)
+
+meuCaixa.pagamento()
+
+
